80aa91eab9e3b02fa4dabe8d57e9359d249cd794
Stefán Carl Peiser <scg2@hi.is>
2015-11-24 13:51:42 +0000

    apis tenging

--- frontscripts/jqueryscript.js
+++ frontscripts/jqueryscript.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   console.log("Stuff's good");
   // Remove navigation buttons from banner
   $('.carousel-indicators ').css("display", "none");
+  setInterval(dataGet(), 5000);
 })
+
+
+function getData() {
+  $.ajax({
+  'url': 'http://apis.is/earthquake/is',
+   success: function(response) {
+    console.log(response);
+    }
+  });
+}
