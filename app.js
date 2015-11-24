494c1f21cd64096bf008eb8fda77c48117ab1a2e
oddgeirpall <doddip123@gmail.com>
2015-11-24 13:11:06 +0000

    mappið virkar, for realz

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(__dirname + '/img'))
 // Command for allowing server access to /frontscripts folder
 app.use('/frontscripts', express.static(__dirname + '/frontscripts'));
+// Command for allowing server access to /public folder
+app.use('/public', express.static(__dirname + '/public'));
 // Command for allowing server access to /bower_components folder
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
