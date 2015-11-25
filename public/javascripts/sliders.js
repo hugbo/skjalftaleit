cda6fa7fb9ee95fa4447c7d8b4b0e9f53813d08c
oddgeirpall <doddip123@gmail.com>
2015-11-25 16:35:52 +0000

    small changes

--- public/javascripts/sliders.js
+++ public/javascripts/sliders.js
@@ -21,5 +21,24 @@ $(document).ready(function() {
 				console.log(sliderData);
 			}
 		});
+		$('slider-time').ionRangeSlider({
+			hide_min_max: true,
+			keyboard: true,
+			min: 0,
+			max: 7,
+			from: 0,
+			to: 7,
+			type: 'double',
+			step: 0.25,
+			grid: true,
+			grid_num: 7,
+			min_interval: 0.5,
+			drag_interval: true,
+			onFinish: function(data) {
+				sliderData = data;
+				console.log(data);
+				console.log(sliderData);
+			}
+		});
 	});
 });
