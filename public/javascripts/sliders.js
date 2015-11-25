b1b889872c6643217975807486e1c3798e80369e
oddgeirpall <doddip123@gmail.com>
2015-11-25 16:55:36 +0000

    ALL THE PRETTY

--- public/javascripts/sliders.js
+++ public/javascripts/sliders.js
@@ -21,19 +21,22 @@ $(document).ready(function() {
 				console.log(sliderData);
 			}
 		});
-		$('slider-time').ionRangeSlider({
+		$('#slider-time').ionRangeSlider({
 			hide_min_max: true,
 			keyboard: true,
-			min: 0,
-			max: 7,
-			from: 0,
-			to: 7,
+			min: +moment().subtract(48, 'hours').format('X'),
+			max: +moment().format('X'),
+			from: +moment().subtract(48, 'hours').format('X'),
+			to: +moment().format('X'),
 			type: 'double',
-			step: 0.25,
 			grid: true,
-			grid_num: 7,
-			min_interval: 0.5,
+			grid_num: 6,
 			drag_interval: true,
+			force_edges: true,
+			prettify: function(num) {
+				var m = moment(num, 'X').locale('is');
+				return m.format('Do MMMM, HH:mm');
+			},
 			onFinish: function(data) {
 				sliderData = data;
 				console.log(data);
