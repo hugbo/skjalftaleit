ba279ea270726da46104924194902f8ff65a61ab
Stefán Carl Peiser <scg2@hi.is>
2015-11-28 15:43:56 +0000

    setInterval issue lagað

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,16 @@ var request = require('request')
 var updateDB = require('../lib/StoreData');
 
 var d = new Date();
-//var startDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + (d.getDate() - 2);
+var startDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + (d.getDate() - 2);
 var endDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + (d.getDate());
-var startDate = "2015-11-21";
 var minMagnitude= 0;
 var maxMagnitude = 10;
-setInterval(getIcelandicQuakeData(), 10*60*1000);
-setInterval(getWorldQuakeData(startDate, endDate, minMagnitude, maxMagnitude), 15*60*1000);
+setInterval(function(){
+  getIcelandicQuakeData();
+}, 5*60*1000);
+setInterval(function(){
+  getWorldQuakeData(startDate, endDate, minMagnitude, maxMagnitude);
+}, 10*60*1000);
 
 
 /* GET and POST Home page. */
@@ -54,13 +57,11 @@ router.get('/data', function(req, res) {
 });
 
 
-
 /*
 ================================================================================
 MIDDLEWARE
 ================================================================================
 */
-
 function getWorldQuakeData(start, end, min, max){
   var originUrl = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&';
   var startTime = '&starttime='+start;
