10199c5536e4e7fecc132f0172753aca9d96a22e
Stefán Carl Peiser <scg2@hi.is>
2015-11-28 21:04:08 +0000

    SQL schema + markdown leiðbeiningar

---
+++ sqlSetup/sqlSetup.js
@@ -0,0 +1,50 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+require('dotenv').load();
+
+var query = require('../lib/query');
+var schema = path.join(__dirname, './earthquakeSchema.sql');
+var pg = require('pg');
+var DATABASE = process.env.DATABASE_URL;
+
+
+console.log('Dropping tables and re-creating...');
+
+fs.readFile(schema, function (err, data) {
+  if (err) {
+    console.error('Error!', err);
+    return;
+  }
+
+  query(data.toString('utf8'), [], function (err) {
+    if (err) {
+      console.error('Error running script!', err);
+      return;
+    }
+
+    console.log('Done!');
+  });
+});
+
+
+function sqlQuery(q, values, cb) {
+  pg.connect(DATABASE, function (error, client, done) {
+
+    if (error) {
+      console.error('Error running query', q, values, error);
+      return cb(error);
+    }
+
+    client.query(q, values, function (err, result) {
+      done();
+
+      if (err) {
+        console.error('Error running query', q, values, err);
+        return cb(err);
+      } else {
+        return cb(null, result);
+      }
+    });
+  });
+};
